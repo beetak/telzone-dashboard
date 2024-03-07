30581f8cdeb712360bac7bed7a503a75f08d18ea
beetak <beetaktj1@gmail.com>
2024-03-07 08:39:58 +0200

    tcflprint

--- src/components/Vouchers/SMSVerification/SMSVerification.js
+++ src/components/Vouchers/SMSVerification/SMSVerification.js
@@ -5,9 +5,15 @@ import { Button, Modal } from 'react-bootstrap';
 import { fetchAsyncSMSVoucher, getAllSales, getLoadingStatus } from '../../../store/sales-slice';
 import FailedAnimation from '../../FailedAnimation/FailedAnimation';
 import TickAnimation from '../../TickAnimation/TickAnimation';
-import { isEmpty } from 'lodash';
+import jsPDF from 'jspdf'
+import "jspdf-autotable";
 import { voucherVerification } from '../../../store/batch-slice';
 const userRole = localStorage.getItem('role')
+const img = "assets/img/telonelogo.png"
+
+const firstname = localStorage.getItem('firstname')
+const surname = localStorage.getItem('surname')
+const shopName = localStorage.getItem('shopName')
 
 const SMSVerification = () => {
 
@@ -55,6 +61,71 @@ const SMSVerification = () => {
     setVoucherCodes([])
   }
 
+  const printVouchers = (e) => {
+    e.preventDefault()
+    const title = "My Awesome Report";
+    const headers = [["","Name", "Voucher Code"]]; 
+    
+    var doc = new jsPDF()
+
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    var today = new Date(),
+    curTime = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    const dateString = new Date(current);
+    const formattedDate = dateString.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' });
+    // var doc = new jsPDF('potrait', 'px', 'a6', 'false')
+    const pageHeight = 170 + (voucherCodes.length * 12); // Calculate the required height based on voucherDetails length
+    var doc = new jsPDF('portrait', 'px', [pageHeight, 160], 'false');
+    // var doc = new jsPDF('potrait', 'px', [280,160], 'false')
+    doc.addImage(img, 'PNG', 15, 5, 70, 25)
+    doc.setFont('Times New Roman', 'bold')
+    doc.setFontSize(12)
+    doc.text(15, 40, 'Headquarters: Runhare House')
+    doc.setFont('Times New Roman', 'regular')
+    doc.setFontSize(10)
+    doc.setTextColor(0,0,0);
+    doc.text(15, 55, '107 Kwame Nkrumah Avenue, Harare, \nZimbabwe\nP.O Box CY 331, Causeway, Harare, \nZimbabwe\n24 Hour Call Center - +263 0242 700950')
+    doc.setFont('Times New Roman', 'bold')
+    doc.setFontSize(12)
+    doc.setTextColor(0,0,0);
+    doc.text(15, 105, 'Client Details')
+    doc.setFont('Times New Roman', 'medium')
+    doc.setFontSize(10)
+    doc.setTextColor(0,0,0);
+    doc.text(15, 115, 'Assignee:  '+phoneNumber)
+    // doc.setFont('Times New Roman', 'bold')
+    // doc.setFontSize(12)
+    // doc.setTextColor(0,0,0);
+    // doc.text(15, 125, 'Receipt Details')
+    // doc.setFont('Times New Roman', 'medium')
+    // doc.setFontSize(10)
+    // doc.setTextColor(0,0,0);
+    // // doc.text(15, 160, 'Receipt Number: ' + orderID )
+    // doc.text(15, 150, 'Date: '+ formattedDate )
+    doc.setFont('Times New Roman', 'bold')
+    doc.setFontSize(12)
+    doc.setTextColor(0,0,0);
+    doc.text(15, 128, 'Cashier Details')
+    doc.setFont('Times New Roman', 'medium')
+    doc.setFontSize(10)
+    doc.setTextColor(0,0,0);
+    doc.text(15, 138, 'Issued By: ' +firstname+ " "+surname+ '\nShop: ' + shopName + "\n")
+
+    doc.setTextColor(0,0,0);
+    doc.setFontSize(10)
+    doc.text(15, 160, 'Bundle: TCFL STUDENTS WI-FI\n')
+
+    voucherCodes.map((item, i)=>{
+      doc.text(
+        15,
+        170 + i * 12,
+        'PIN: ' + item
+      )
+    });
+    doc.save('invoice.pdf')
+  }
+
   let loadingAnimation =
     <div className='text-center' style={anime}>
       <h5 style={{color: '#055bb5', overflow: 'hidden'}}>{
@@ -96,7 +167,8 @@ const handleVerification = async (item) => {
               </>
             })          
         }
-        <button onClick={clearVouchers} className="btn btn-info my-2">Clear</button>
+          <button onClick={printVouchers} className="btn btn-info my-2">Print</button>
+          <button onClick={clearVouchers} className="btn btn-danger my-2 ms-2">Clear</button>
       </div>
   }
   else{
@@ -119,7 +191,7 @@ const handleVerification = async (item) => {
               <form >
                 <label className="form-label">Mobile Number</label>
                 <div className="input-group input-group-dynamic mb-4">
-                  <input type="text" name="phoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="form-control" placeholder='Enter Voucher Code'/>
+                  <input type="text" name="phoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="form-control" placeholder='Enter Mobile Number'/>
                 </div>
                 {
                   loading ?
